a3a2032af10782b4a8c61f47965a00866099c1e4
oussama <hachibi.killerbe@gmail.com>
2020-06-19 10:54:05 +0100

    Updates

---
+++ precache-manifest.aad0b3dd70ff8029fbcbb589827de946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560b2647e06f7ac460631cccd1a531d6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "1814357774f84816be83",
+    "url": "/todolist/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "893a64195db495892afb",
+    "url": "/todolist/static/js/2.0c092fe6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.0c092fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1814357774f84816be83",
+    "url": "/todolist/static/js/main.ee019ed5.chunk.js"
+  },
+  {
+    "revision": "130fd48b1b39e8c6b47c",
+    "url": "/todolist/static/js/runtime-main.5f712ceb.js"
+  }
+]);
